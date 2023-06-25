b251e7709b01e7113c09e3b585134fc6f13bb5bd
Oscar Abreu <oja227@nyu.edu>
2023-06-24 20:54:46 -0400

    Final (Missing: Comments)

---
+++ recipe.js
@@ -0,0 +1,61 @@
+const result = document.getElementById("result");
+const searchButton = document.getElementById("search");
+const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+searchButton.addEventListener("click", () => {
+  let userInput = document.getElementById("user-input").value;
+  if (userInput.length == 0) {
+    result.innerHTML = `<h3>Come on, pick your favorite recipe!</h3>`;
+  } else {
+    fetch(url + userInput)
+      .then((response) => response.json())
+      .then((data) => {
+        let myMeal = data.meals[0];
+        let count = 1;
+        let ingredients = [];
+        for (let i in myMeal) {
+          let ingredient = "";
+          let measure = "";
+          if (i.startsWith("strIngredient") && myMeal[i]) {
+            ingredient = myMeal[i];
+            measure = myMeal[`strMeasure` + count];
+            count += 1;
+            ingredients.push(`${measure} ${ingredient}`);
+          }
+        }
+        console.log(ingredients);
+        result.innerHTML = 
+        `<img src=${myMeal.strMealThumb}>
+         <div class="details">
+            <h2>${myMeal.strMeal}</h2>
+            <h4>${myMeal.strArea}</h4>
+        </div>
+        <div id="ingredient-con"></div>
+        <div id="recipe">
+            <button id="hide-recipe">X</button>
+            <pre id="instructions">${myMeal.strInstructions}</pre>
+        </div>
+        <button id="show-recipe">View Recipe</button>`;
+        let ingredientCon = document.getElementById("ingredient-con");
+        let parent = document.createElement("ul");
+        let recipe = document.getElementById("recipe");
+        let hideRecipe = document.getElementById("hide-recipe");
+        let showRecipe = document.getElementById("show-recipe");
+        ingredients.forEach((i) => {
+          let child = document.createElement("li");
+          child.innerText = i;
+          parent.appendChild(child);
+          ingredientCon.appendChild(parent);
+        });
+        hideRecipe.addEventListener("click", () => {
+          recipe.style.display = "none";
+        });
+        showRecipe.addEventListener("click", () => {
+          recipe.style.display = "block";
+        });
+      })
+      .catch(() => {
+        result.innerHTML = `<h3>Invalid Input</h3>`;
+      });
+  }
+});
