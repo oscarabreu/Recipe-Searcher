f17a88fb66e8770833c0ce368a2d4d81a5408213
Oscar Abreu <99779654+oscarabreu@users.noreply.github.com>
2023-10-14 11:29:51 -0400

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,20 +1,25 @@
-// 
-
+// Get references to HTML elements
 const result = document.getElementById("result");
 const searchButton = document.getElementById("search");
 const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
+// Add click event listener to the search button
 searchButton.addEventListener("click", () => {
+  // Get user input from the input field
   let userInput = document.getElementById("user-input").value;
   if (userInput.length == 0) {
+    // Display a message if the user input is empty
     result.innerHTML = `<h3>Come on, pick your favorite recipe!</h3>`;
   } else {
+    // Make a fetch request to the API with user input
     fetch(url + userInput)
       .then((response) => response.json())
       .then((data) => {
+        // Extract data for the first meal
         let myMeal = data.meals[0];
         let count = 1;
         let ingredients = [];
+        // Loop through meal properties to get ingredients and measures
         for (let i in myMeal) {
           let ingredient = "";
           let measure = "";
@@ -25,6 +30,7 @@ searchButton.addEventListener("click", () => {
             ingredients.push(`${measure} ${ingredient}`);
           }
         }
+        // Display meal details and ingredients
         result.innerHTML = 
         `<img src=${myMeal.strMealThumb}>
          <div class="details">
@@ -42,12 +48,14 @@ searchButton.addEventListener("click", () => {
         let recipe = document.getElementById("recipe");
         let hideRecipe = document.getElementById("hide-recipe");
         let showRecipe = document.getElementById("show-recipe");
+        // Create and display the list of ingredients
         ingredients.forEach((i) => {
           let child = document.createElement("li");
           child.innerText = i;
           parent.appendChild(child);
           ingredientCon.appendChild(parent);
         });
+        // Add event listeners to show/hide recipe instructions
         hideRecipe.addEventListener("click", () => {
           recipe.style.display = "none";
         });
@@ -56,6 +64,7 @@ searchButton.addEventListener("click", () => {
         });
       })
       .catch(() => {
+        // Handle errors by displaying an "Invalid Input" message
         result.innerHTML = `<h3>Invalid Input</h3>`;
       });
   }
